303f090de9a1c8d5cccfff291ab9e8b7cd3748df
aashish subedi <aashishsubedi105@gmail.com>
2023-10-01 09:26:51 +0530

    updated

---
+++ Leetcode/557.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    string reverseWords(string &s)
+    {
+        int i = 0;
+        for (int j = 0; j < s.size() + 1; ++j)
+        {
+            if (s[j] == ' ' || j == s.length())
+            {
+                reverse(s.begin() + i, s.begin() + j);
+                i = j + 1;
+            }
+        }
+
+        return s;
+    }
+};
